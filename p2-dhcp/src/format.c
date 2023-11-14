c780a8fe7884242efe3d34923c6df6a3332a4344
Andrew Davis <davis7aj@dukes.jmu.edu>
2023-11-13 21:43:25 -0500

    progress

--- p2-dhcp/src/format.c
+++ p2-dhcp/src/format.c
@@ -12,6 +12,79 @@ dump_msg (FILE *output, msg_t *msg, size_t size)
 
   // TODO: Print out the BOOTP fields as specified in the assignment
 
+  if (msg->op == 1)
+  {
+    printf("Op Code (op) = %hhu [BOOTREQUEST]\n", msg->op);
+  } else {
+    printf("Op Code (op) = %hhu [BOOTREPLY]\n", msg->op);
+  }
+
+   switch (msg->htype) {
+        case 1:
+            printf("Hardware Type (htype) = %hhu [Ethernet (10Mb)]\n", msg->htype);
+            break;
+        case 6:
+            printf("Hardware Type (htype) = %hhu [IEEE 802 Network]\n", msg->htype);
+            break;
+        case 7:
+            printf("Hardware Type (htype) = %hhu [ARCNET]\n", msg->htype);
+            break;
+        case 15:
+            printf("Hardware Type (htype) = %hhu [Frame Relay]\n", msg->htype);
+            break;
+        case 18:
+            printf("Hardware Type (htype) = %hhu [Fibre Channel]\n", msg->htype);
+            break;
+        case 19:
+            printf("Hardware Type (htype) = %hhu [ATM]\n", msg->htype);
+            break;
+        // Add more cases as needed
+        default:
+            printf("Unknown hardware type (%u).\n", msg->htype);
+            break;
+    }
+
+    printf("Hardware Address Length (hlen) = %hhu\n", msg->hlen);
+    printf("Hops (hops) = %hhu\n", msg->hops);
+    printf("Transaction ID (xid) = %u (0x%X)\n", msg->xid, msg->xid);
+
+
+    int seconds = msg->secs % 60;
+    int totalMinutes = msg->secs / 60;
+    int minutes  = totalMinutes % 60;
+    int hours = totalMinutes / 60;
+
+    printf("Seconds (secs) = 0 Days, %d:%d:%d\n", hours, minutes, seconds);
+    printf("Flags (flags) = %u\n", msg->flags);
+    // Print ciaddr in dotted decimal notation
+    printf("Client IP Address (ciaddr) = %u.%u.%u.%u\n",
+           (msg->ciaddr >> 24) & 0xFF,
+           (msg->ciaddr >> 16) & 0xFF,
+           (msg->ciaddr >> 8) & 0xFF,
+           msg->ciaddr & 0xFF);
+
+    printf("Your IP Address (yiaddr) = %u.%u.%u.%u\n",
+           (msg->yiaddr >> 24) & 0xFF,
+           (msg->yiaddr >> 16) & 0xFF,
+           (msg->yiaddr >> 8) & 0xFF,
+           msg->yiaddr & 0xFF);
+    
+    printf("Server IP Address (siaddr) = %u.%u.%u.%u\n",
+           (msg->siaddr >> 24) & 0xFF,
+           (msg->siaddr >> 16) & 0xFF,
+           (msg->siaddr >> 8) & 0xFF,
+           msg->siaddr & 0xFF);
+
+    printf("Relay IP Address (giaddr) = %u.%u.%u.%u\n",
+           (msg->giaddr >> 24) & 0xFF,
+           (msg->giaddr >> 16) & 0xFF,
+           (msg->giaddr >> 8) & 0xFF,
+           msg->giaddr & 0xFF);
+
+    printf("Client Ethernet Address (chaddr) = %X\n", msg->chaddr);
+
+
+
   fprintf (output, "------------------------------------------------------\n");
   fprintf (output, "DHCP Options\n");
   fprintf (output, "------------------------------------------------------\n");
--- p2-dhcp/src/interp.c
+++ p2-dhcp/src/interp.c
@@ -1,5 +1,12 @@
 #include <stdlib.h>
 
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
 #include "dhcp.h"
 #include "format.h"
 
@@ -8,19 +15,66 @@ main (int argc, char **argv)
 {
   // TODO: Open the file specified on the command line
   // Note: The file contains a binary network packet
+  int file = open(argv[1], O_RDONLY);
+
+  if (file == -1) {
+    perror("couldnt open file");
+    return 1;
+  }
+
+  // Open the file
+    FILE *filePointer = fopen(argv[1], "r"); 
+
+    if (filePointer == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
 
   // TODO: Determine the size of the packet in bytes
   // Note: You have used the fstat() function before
 
+  struct stat fileStat;
+  if (fstat(file, &fileStat) == -1)
+  {
+    perror("couldnt get file info");
+    close(file);
+    return 1;
+  }
+
   // TODO: Define a uint8_t * variable
+  uint8_t *packBuf;
 
   // TODO: Allocate enough space to hold the packet (store in your varaible)
+  packBuf = (uint8_t *)calloc(fileStat.st_size, sizeof(uint8_t));
 
   // TODO: Read the packet data from the file into your variable
+  ssize_t nbRead = read(file, packBuf, fileStat.st_size);
+  if (nbRead ==-1)
+  {
+    perror("err reading fle");
+    free(packBuf);
+    close(file);
+    return 1;
+  }
+  
+  msg_t dhcpMsg;
+
+  //copy contents to struct
+  // if (memcpy(&dhcpMsg,  packBuf, sizeof(packBuf)) != 0)
+  // {
+  //   /* code */
+  //   fprintf(stderr, "ERROR cpy data to struct using memcpy_s");
+  //   return 1;
+  // }
+  memcpy(&dhcpMsg,  packBuf, sizeof(packBuf));
+  
 
   // TODO: Call dump_msg() from format.c to print the packet data to stdout
-
+  dump_msg(filePointer, &dhcpMsg, sizeof(packBuf));
   // TODO: Don't forget to free the dynamically-allocated space
 
+  close(file);
+  fclose(filePointer);
+  free(packBuf);
   return EXIT_SUCCESS;
 }
