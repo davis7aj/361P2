8cd89fc0b3e84c7723c53d2e9e3304fb7afefd3b
ChrisDoch <joshawa220@gmail.com>
2023-11-16 14:49:32 -0500

    updated client

--- p2-dhcp/src/client.c
+++ p2-dhcp/src/client.c
@@ -13,7 +13,7 @@ main (int argc, char **argv)
 static bool
 get_args (int argc, char **argv)
 {
-  uint32_t x = 42;
+  uint32_t x = strtol(42);
   int t = ETH;
   uint8_t c[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
   int m = DHCPDISCOVER;
@@ -27,7 +27,7 @@ get_args (int argc, char **argv)
   memcpy(msg.op, m);
   memcpy(msg.htype, t);
   memcpy(msg.hlen, 16);
-  memcpy(msg.xid, x);
+  memcpy(msg.xid, htonl(x));
   memcpy(msg.ciaddr, r);
   memcpy(msg.siaddr, s);
   memcpy(msg.chaddr, c);
@@ -36,13 +36,14 @@ get_args (int argc, char **argv)
     if (strcmp(argv[index], "-x") == 0)
     {
       x = (uint32_t) strtol(argv[index + 1], (char **)NULL, 10);
-      memcpy(msg.xid, x);
+      memcpy(msg.xid, htonl(x));
       index += 2;
     }
     if (strcmp(argv[index], "-t") == 0)
     {
       t = (int) strtol(argv[index + 1], (char **)NULL, 10);
       memcpy(msg.htype, t);
+      memcpy(msg.hlen, strlen(argv[index + 1]));
       index += 2;
     }
     if (strcmp(argv[index], "-c") == 0)
@@ -74,16 +75,15 @@ get_args (int argc, char **argv)
     }
     if (strcmp(argv[index], "-p") == 0)
     {
-      x = 0;
+      x = 0; //remove after
+      memcpy(msg.xid, x); //remove after
       int socketfd = socket (AF_INET, SOCK_DGRAM, 0);
       struct sockaddr_in address;
       address.sin_family = AF_INET;
       address.sin_port = (uint16_t) strtol(get_port(), (char **)NULL, 10);
-      in_addr_t val;
-      inet_aton("127.0.0.1", &val);
-      address.sin_addr = val;
+      address.sin_addr = s;
       address.sin_zero = 0;
-      
+      dump_msg(stdout, &msg, sizeof(msg));
       index += 1;
     }
   }
