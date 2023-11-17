952fd5826ffe48f3996c636f74b661c6b2930902
ChrisDoch <joshawa220@gmail.com>
2023-11-16 20:38:29 -0500

    fixed a bunch of client bugs

--- p2-dhcp/src/client.c
+++ p2-dhcp/src/client.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
 #include "dhcp.h"
 #include "format.h"
@@ -10,6 +12,9 @@
 static bool
 get_args (int argc, char **argv)
 {
+  bool p = false;
+  int socketfd;
+  struct sockaddr_in address;
   uint32_t x = strtol("42", (char **)NULL, 10);
   int t = ETH;
   uint8_t c[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
@@ -18,15 +23,13 @@ get_args (int argc, char **argv)
   inet_aton("127.0.0.1", &s);
   struct in_addr r;
   inet_aton("127.0.0.2", &r);
-  int index = 0;
+  int index = 1;
   msg_t msg;
   memset(&msg, 0, sizeof(msg));
-  memcpy(&msg.op, &m, sizeof(m));
-  memcpy(&msg.htype, &t, sizeof(t));
-  int length = 16;
-  memcpy(&msg.hlen, &length, 16);
-  x = htonl(x);
-  memcpy(&msg.xid, &x, sizeof(x));
+  msg.op = m;
+  msg.htype = t;
+  msg.hlen = ETH_LEN;
+  msg.xid = htonl(x);
   msg.ciaddr.s_addr = r.s_addr;
   msg.siaddr.s_addr = s.s_addr;
   memcpy(&msg.chaddr, &c, sizeof(c));
@@ -36,18 +39,17 @@ get_args (int argc, char **argv)
     {
       x = (uint32_t) strtol(argv[index + 1], (char **)NULL, 10);
       x = htonl(x);
-      memcpy(&msg.xid, &x, sizeof(x));
+      msg.xid = x;
       index += 2;
     }
-    if (strcmp(argv[index], "-t") == 0)
+    else if (strcmp(argv[index], "-t") == 0)
     {
       t = (int) strtol(argv[index + 1], (char **)NULL, 10);
       memcpy(&msg.htype, &t, sizeof(t));
-      length = strlen(argv[index + 1]);
-      memcpy(&msg.hlen, &length, strlen(argv[index + 1]));
+      msg.hlen = (int) strtol(argv[index + 1], (char **)NULL, 10); ;
       index += 2;
     }
-    if (strcmp(argv[index], "-c") == 0)
+    else if (strcmp(argv[index], "-c") == 0)
     {
       for (int i = 0; i < strlen(argv[index]); i++)
       {
@@ -56,38 +58,48 @@ get_args (int argc, char **argv)
       memcpy(&msg.chaddr, &c, sizeof(c));
       index += 2;
     }
-    if (strcmp(argv[index], "-m") == 0)
+    else if (strcmp(argv[index], "-m") == 0)
     {
       m = (int) strtol(argv[index + 1], (char **)NULL, 10);
-      memcpy(&msg.op, &m, sizeof(m));
+      msg.op = m;
       index += 2;
     }
-    if (strcmp(argv[index], "-s") == 0)
+    else if (strcmp(argv[index], "-s") == 0)
     {
       inet_aton(argv[index + 1], &s);
       msg.siaddr.s_addr = s.s_addr;
       index += 2;
     }
-    if (strcmp(argv[index], "-r") == 0)
+    else if (strcmp(argv[index], "-r") == 0)
     {
       inet_aton(argv[index + 1], &r);
       msg.ciaddr.s_addr = r.s_addr;
       index += 2;
     }
-    if (strcmp(argv[index], "-p") == 0)
+    else if (strcmp(argv[index], "-p") == 0)
     {
       x = htonl(0); //remove after
       memcpy(&msg.xid, &x, sizeof(x)); //remove after
-      int socketfd = socket (AF_INET, SOCK_DGRAM, 0);
-      struct sockaddr_in address;
+      socketfd = socket (AF_INET, SOCK_DGRAM, 0);
       memset(&address, 0, sizeof(address));
       address.sin_family = AF_INET;
       address.sin_port = (uint16_t) strtol(get_port(), (char **)NULL, 10);
       address.sin_addr.s_addr = s.s_addr;
-      dump_msg(stdout, &msg, sizeof(msg));
-      sendto(socketfd, &msg, sizeof(msg), 0, &address, sizeof(address));
+      // dump_msg(stdout, &msg, sizeof(msg));
+      p = true;
       index += 1;
     }
+    else
+    {
+      printf("Error encountered at : %s\n", argv[index]);
+      return false;
+    }
+  }
+  dump_msg(stdout, &msg, sizeof(msg));
+  if (p)
+  {
+    sendto(socketfd, &msg, sizeof(msg), 0, (struct sockaddr *) &address, sizeof(address));
+    close(socketfd);
   }
   return true;
 }
